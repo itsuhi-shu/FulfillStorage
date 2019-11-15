ef4389c586c30e1af5ca96d226d5bbd577175bf2
itsuhi-shu <zhouyifeig@gmail.com>
2019-11-15 10:52:58 +0900

    Initial Commit

---
+++ FullfilStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FullfilStorage
+//
+//  Created by 周 軼飛 on 2019/11/15.
+//  Copyright © 2019 ZHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
